06d72122a7af8e56f42a4d3308a078eeda94d99f
LeoMatiass <80693740+LeoMatiass@users.noreply.github.com>
2021-05-20 19:01:48 -0300

    Delete Atividades[Presença] - Etapa 1 directory

--- Atividades[Presença] - Etapa 1/Atividade 2/ListaEncadeada.py
+++
@@ -1,6 +0,0 @@
-le = [1, 2, 3, 4]
-le.insert(5, 6)
-print("adicionando números na lista: ", le)
-print('----------------------------------------------------------')
-le.pop(4)
-print("removendo numeros na lista: ", le)
--- Atividades[Presença] - Etapa 1/Atividade 3/MegaSena.py
+++
@@ -1,4 +0,0 @@
-import random
-print("Numeros aleatorios da Mega Sena.\n")
-for i in range(1,7):
-    print("numero: ",random.randint(1,60))
